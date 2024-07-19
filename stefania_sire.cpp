7f600695373f674a53912f83cc43a7ae70cbc229
Felix Soubelet <felix.soubelet@protonmail.com>
2024-07-19 14:46:45 +0200

    remove the printouts as cell attribution was figured out

--- stefania_sire.cpp
+++ stefania_sire.cpp
@@ -3848,7 +3848,6 @@ int IBS(void)
   double maxx,minx,maxz,minz,maxs,mins;
 //  double deltacell,deltacellx,deltacellz,deltacells;
   double density,totx,totz,tots;
-  double valx, valz, vals;  // TODO: remove after debugging
   int cont,cont2,nump,scatterflag,dummy,dummy1,dummy2;
   int *cell,*npart,**part,*ncol; 
   int flag,comodino,j1,j2,conteur=0,limit1,limit2,ncolcel;  
@@ -3981,30 +3980,10 @@ int IBS(void)
   
   for(cont=0;cont<numpart;cont++) {
       // characteristic integer for each macroparticle. Macroparticles in the same cell will have the same integer (I think this based on a PIC (particles-in-cell) algorithmo (???) )
-      cout << "x[cont] = " << x[cont] << endl;  // for reproduction / debugging
-	  cout << "z[cont] = " << z[cont] << endl;  // for reproduction / debugging
-	  cout << "deltasp[cont] = " << deltasp[cont] << endl;  // for reproduction / debugging
-	  valx = (x[cont] - minx) / deltacellx;
-      valz = (z[cont] - minz) / deltacellz;
-      vals = (deltasp[cont] - mins) / deltacells;
-	  cout << "valx = " << valx << endl;  // for reproduction / debugging
-	  cout << "valz = " << valz << endl;  // for reproduction / debugging
-	  cout << "vals = " << vals << endl;  // for reproduction / debugging
-	  cout << "floor(valx) = " << floor(valx) << endl;  // for reproduction / debugging
-	  cout << "floor(valz) = " << floor(valz) << endl;  // for reproduction / debugging
-	  cout << "floor(vals) = " << floor(vals) << endl;  // for reproduction / debugging
-	  cout << "(int)floor(valx) = " << (int)floor(valx) << endl;  // for reproduction / debugging
-	  cout << "(int)floor(valz) = " << (int)floor(valz) << endl;  // for reproduction / debugging
-	  cout << "(int)floor(vals) = " << (int)floor(vals) << endl;  // for reproduction / debugging
-      cell[cont] = (int)floor(valx) + (int)floor(valz) * ncellx + (int)floor(vals) * ncellt;
-    //   cell[cont] = ((int)floor((x[cont] - minx) / deltacellx))
-	//                + ((int)floor((z[cont] - minz) / deltacellz)) * ncellx
-	//                + ((int)floor((deltasp[cont] - mins) / deltacells)) * ncellt;
-      cout << "res = " << ((int)floor((x[cont] - minx) / deltacellx))
+      cell[cont] = ((int)floor((x[cont] - minx) / deltacellx))
 	               + ((int)floor((z[cont] - minz) / deltacellz)) * ncellx
-	               + ((int)floor((deltasp[cont] - mins) / deltacells)) * ncellt << endl;
+	               + ((int)floor((deltasp[cont] - mins) / deltacells)) * ncellt;
 	  npart[cell[cont]]++; // counts the number of macroparticles with the same integer       
-      cout << "stored cell value = " << cell[cont] << endl;  // for reproduction / debugging
 	}
 
   // For benchmark purposes I print out this array
